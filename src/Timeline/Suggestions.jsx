0ce67a24ea686bf7de2c32de2f0e7eb0445b43e6
Shakil Khan <m.shakilkhan720@gmail.com>
2023-11-17 00:26:05 +0600

    suggested part implemented

--- src/Timeline/Suggestions.jsx
+++ src/Timeline/Suggestions.jsx
@@ -1,10 +1,107 @@
 import React from 'react'
 import "./Suggestions.css";
+import { Avatar } from '@mui/material';
 
 function Suggestions() {
   return (
-    <div>
-      Suggestions
+    <div className='suggestions'>
+      <div className="suggestion__title">
+        Suggested for you
+      </div>
+      <div className="suggestions_usernames">
+
+        <div className="suggestion__username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className='username'>shakil_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="suggestion__username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className='username'>shakil_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="suggestion__username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className='username'>shakil_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="suggestion__username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className='username'>shakil_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="suggestion__username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className='username'>shakil_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="suggestion__username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className='username'>shakil_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="suggestion__username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className='username'>shakil_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+      </div>
     </div>
   )
 }
